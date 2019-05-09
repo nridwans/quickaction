bb8992421c08ff48d340382cd0b4b71cdcf3821a
ridwansusanto <xa18.ridwan@gmail.com>
2019-05-09 14:52:38 +0700

    - extend popupwindow

--- quickaction/src/main/java/me/piruin/quickaction/PopupWindows.java
+++ quickaction/src/main/java/me/piruin/quickaction/PopupWindows.java
@@ -27,18 +27,14 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.PopupWindow;
 
-class PopupWindows {
-  PopupWindow mWindow;
-  private View mRootView;
-  private Context mContext;
+class PopupWindows extends PopupWindow {
 
   PopupWindows(Context context) {
-    mContext = context;
-    mWindow = new PopupWindow(context);
-    mWindow.setTouchInterceptor(new View.OnTouchListener() {
+    super(context);
+    setTouchInterceptor(new View.OnTouchListener() {
       @Override public boolean onTouch(View view, MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
-          mWindow.dismiss();
+          dismiss();
           return true;
         } else if (event.getAction() == MotionEvent.ACTION_BUTTON_PRESS) {
           view.performClick();
@@ -49,47 +45,18 @@ class PopupWindows {
     });
   }
 
-  Context getContext() {
-    return mContext;
-  }
-
   void preShow() {
-    if (mRootView == null)
-      throw new IllegalStateException("setContentView was not called with a view to display.");
-
-    mWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-    mWindow.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
-    mWindow.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
-    mWindow.setTouchable(true);
-    mWindow.setFocusable(true);
-    mWindow.setOutsideTouchable(true);
-    mWindow.setContentView(mRootView);
+    setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
+    setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+    setTouchable(true);
+    setFocusable(true);
+    setOutsideTouchable(true);
     setShadows();
   }
 
   @TargetApi(Build.VERSION_CODES.LOLLIPOP) private void setShadows() {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-      mWindow.setElevation(10);
-  }
-
-  void setContentView(View root) {
-    mRootView = root;
-    mWindow.setContentView(root);
-  }
-
-  public View getContentView() {
-    return mRootView;
-  }
-
-  public boolean isShowing() {
-    return mWindow.isShowing();
-  }
-
-  void setOnDismissListener(PopupWindow.OnDismissListener listener) {
-    mWindow.setOnDismissListener(listener);
-  }
-
-  public void dismiss() {
-    mWindow.dismiss();
+      setElevation(10);
   }
 }
